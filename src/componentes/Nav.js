d5b0e0351ffadb9bf18a75cb3396b9a217461ed9
hector silva <hectorsf21@gmail.com>
2023-01-12 10:16:20 +0100

    responsive

--- src/componentes/Nav.js
+++ src/componentes/Nav.js
@@ -3,18 +3,24 @@ import logo from '../img/logo.png'
 import style from '../componentes/styles/Nav.module.css'
 import BuscarPeliculasNav from './BuscarPeliculasNav'
 export default function Nav() {
-  return (
-    <div>
-        <div className={style.contentNav}>
-            <div className={style.subContent}>
-                <div className={style.contentImage}>
-                    <img src={logo} alt='logo'/>
-                </div>
-                <div>
-                    <BuscarPeliculasNav/>
+    return (
+        <div>
+            <div className={style.contentNav}>
+                <div className='row'>
+                    <div className={style.subContent}>
+                        <div className='col-12 text-center text-lg-start col-lg-6'>
+                            <div className={style.contentImage}>
+                                <img src={logo} alt='logo' />
+                            </div>
+                        </div>
+                        <div className='col-12 col-lg-6'>
+                            <div className={style.boxSearch}>
+                                <BuscarPeliculasNav />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
